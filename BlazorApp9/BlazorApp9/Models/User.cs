3430c63579618053204c873cf80b76cae14c6379
juttu <juttu@LAPTOP-UT3MT0EG>
2025-04-19 22:33:58 +0530

    Add project files.

---
+++ BlazorApp9/BlazorApp9/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using BlazorApp1.DBConnections;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace BlazorApp1.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Username")]
+        public string Username { get; set; }
+
+        [BsonElement("Password")]
+        public string Password { get; set; }
+
+        [BsonElement("Email")]
+        public string Email { get; set; }
+    }
+
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(string connectionString, string databaseName)
+        {
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+
+        public IMongoCollection<Favorite> Favorites => _database.GetCollection<Favorite>("Favorites");
+    }
+
+}
